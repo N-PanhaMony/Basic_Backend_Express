6eecd4a49f4d852dd7e7d982b3519c8cec02dcef
N-PanhaMony <neangpanhamony001@gmail.com>
2025-11-12 21:33:00 +0700

    Add newUser from frond

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const express = require('express')
 const app = express()
 const PORT = 8080
 
-let data ={
-    id : 1,
-    'name' : 'Bot'
-}
+let data = ['bot1']
 
+//middleware
+app.use(express.json())
 
 // ENDPOINT - HTTP VERBS (method) && Routes (or paths)
 // The method informs the nature of request and the route is a further subdirectory (basically we direct the request to the body of code to respond appropriately, and these locations or routes are called endpoints)
@@ -14,12 +13,25 @@ let data ={
 // Type 1 - Website endpoints (these endpoints are for sending back html and they typically come when a user enters a url in a browser)
 
 app.get('/', (req,res) =>{
-    res.send('<h1>Home</h1>')
+    res.send(`
+    <body>
+        <h2>Data:</h2>
+        <p>${JSON.stringify(data)}</p>
+    </body>`)    
 })
 
 // Type 2 - API Endpoint 
 app.get('/api/data', (req, res) =>{
     res.send(data)
+})
+
+//CRUD-method create-post read-get update-put and delete-delete
+app.post('/api/data', (req,res) => {
+    const newEntry = req.body
+    console.log(newEntry);
+    data.push(newEntry.name)
+    res.sendStatus(201)
+
 })
 
 app.listen(PORT , () => console.log(`server listen to port ${PORT}`))
